eda4e52379c14fa358ea894e4ed936b89afd4263
Damian Tarnawsky <damian@builder.io>
2025-01-28 11:25:41 -0800

    initial

---
+++ app/routes/($slug)._index.tsx
@@ -0,0 +1,47 @@
+// ($slug)._index.tsx
+import {
+  Content,
+  fetchOneEntry,
+  getBuilderSearchParams,
+  isPreviewing,
+} from '@builder.io/sdk-react';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { fetch as webFetch } from '@remix-run/web-fetch';
+
+const apiKey = '8e7687fd137b45d5b54a73ea0c954be3'; // Replace with your actual API key
+
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const urlPath = `/${params['slug'] || ''}`;
+  console.log(urlPath)
+
+  const { initializeNodeRuntime } = await import('@builder.io/sdk-react/node/init');
+  await initializeNodeRuntime();
+
+  const page = await fetchOneEntry({
+    model: 'page',
+    apiKey: apiKey,
+    options: getBuilderSearchParams(url.searchParams),
+    userAttributes: { urlPath },
+    fetch: webFetch,
+  });
+
+  if (!page && !isPreviewing(url.search)) {
+    throw new Response('Page Not Found', {
+      status: 404,
+      statusText: 'Page not found in Builder.io',
+    });
+  }
+
+  return { page };
+};
+
+// Define and render the page.
+export default function Page() {
+  // Use the useLoaderData hook to get the Page data from `loader` above.
+  const { page } = useLoaderData<typeof loader>();
+
+  // Render the page content from Builder.io
+  return <Content model="page" apiKey={apiKey} content={page as any} />;
+}
